e516b15512f3c54b5c135ce725d575003a0964ca
MihaplAyMF <mihapley@gmail.com>
2023-03-19 14:55:45 +0200

    Ще трохи погрався

--- Main.cpp
+++ Main.cpp
@@ -4,18 +4,12 @@
 
 #include "function.h"
 #include "resource.h"
-
 HINSTANCE hinstance_app;
-
-//BOOL TextOut(HDC hdc, int nxStart, int nyStart, LPCTSTR lpString, int cbString);
-
-
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow) {
   
     srand(time(nullptr));
 
-    // Register the window class.
     const wchar_t CLASS_NAME[] = L"Sample Window Class";
     hinstance_app = hInstance;
 
@@ -27,11 +21,8 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
     
     wc.hIcon = LoadIconA(hInstance, MAKEINTRESOURCEA(IDI_ICON1));
     wc.hCursor = LoadCursor(hInstance, MAKEINTRESOURCE(IDC_CURSOR1));
-    //wc.lpszMenuName = MAKEINTRESOURCE(IDR_MENU1);
-
+    
     RegisterClass(&wc);
-
-    // Create the window.
 
     HWND hwnd = CreateWindowEx(
         0,                              // Optional window styles.
@@ -57,13 +48,6 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
 
     MSG msg = { };
 
-    RECT rect;
-    HDC hdc = GetDC(hwnd);
-    COLORREF old_fcolor, old_bcolor;
-    
-    GetClientRect(hwnd, &rect);
-    FillRect(hdc, &rect, (HBRUSH)(COLOR_WINDOW + 2)); 
-    SetBkMode(hdc, TRANSPARENT);
     while (TRUE) {
         if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)){
 
@@ -72,17 +56,7 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
             DispatchMessage(&msg);
 
         } 
-      
-      
-        SetTextColor(hdc, RGB(rand() % 256, rand() % 256, rand() % 256));
-        
-        TextOut(hdc, randint(0, 400), randint(0, 400), L"Hello world!", strlen("Hello world!"));
-
-        Sleep(100);
-
     }
-
-    ReleaseDC(hwnd, hdc);
 
     return 0;
 }
@@ -96,45 +70,64 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
     int virtual_code = (int)wParam;
     int key_state = (int)lParam;
 
+    char ascii_code = wParam;
+    //unsigned int key_state = lParam;
 
-    COLORREF old_fcolor, old_bcolor; 
-    int old_tmode;
+    int width = LOWORD(lParam);
+    int height = HIWORD(lParam);
+
+    static int wm_paint_count = 0;
+    char text[80] = { '\0' };
+    char text2[80] = { '\0' };
+    char text3[80] = { '\0' };
+    char text4[80] = { '\0' };
 
     switch (uMsg) {
 
-
         case WM_CREATE: 
-            break;
-        case WM_PAINT: 
-                
-
-            GetClientRect(hwnd, &rect); 
-            hdc = GetDC(hwnd); 
-
-           //FillRect(hdc, &rect, (HBRUSH)(COLOR_WINDOW + 2));
-
-            /*old_fcolor = SetTextColor(hdc, RGB(0, 255, 0));
-            old_bcolor = SetBkColor(hdc, RGB(0, 0, 0));
-            old_tmode = SetBkMode(hdc, TRANSPARENT);
-
-            SetTextColor(hdc, RGB(rand() % 256, rand() % 256, rand() % 256));
-            SetBkColor(hdc, RGB(0, 0, 0));
+            
+            hdc = GetDC(hwnd);
+            GetClientRect(hwnd, &rect);
+        
+            SetTextColor(hdc, RGB(20, 220, 20));
             SetBkMode(hdc, TRANSPARENT);
 
-            TextOut(hdc, randint(0, 400), randint(0, 400), L"HelloWorld", strlen("HelloWorld"));
-            TextOut(hdc, 50, 80, L"HelloWorld", strlen("HelloWorld"));
+            FillRect(hdc, &rect, (HBRUSH)(COLOR_WINDOW + 2));
+
+            sprintf(text, "Draw number = %d ", ++wm_paint_count);
+            TextOutA(hdc, 10, 20, text, strlen(text));
+
+            sprintf(text2, "WM_SIZE Called - New Size = %d, %d ", width, height);
+            TextOutA(hdc, 10, 40, text2, strlen(text2));
+
+            sprintf(text3, "WM_CHAR: Character = %c ", ascii_code);
+            TextOutA(hdc, 10, 60, text3, strlen(text3));
+            ReleaseDC(hwnd, hdc);
+            ValidateRect(hwnd, &rect);
+            break;
+        case WM_PAINT: 
+
+            hdc = GetDC(hwnd); 
+            GetClientRect(hwnd, &rect); 
             
-            Sleep(100);*/
+            FillRect(hdc, &rect, (HBRUSH)(COLOR_WINDOW + 2));
+            
+            SetTextColor(hdc, RGB(20, 220, 20));
+            SetBkMode(hdc, TRANSPARENT);
+
+            sprintf(text, "Draw number = 0 ", ++wm_paint_count);
+            TextOutA(hdc, 10, 20, text, strlen(text));
+
+            sprintf(text2, "WM_SIZE Called - New Size = %d, %d ", width, height);
+            TextOutA(hdc, 10, 40, text2, strlen(text2));
 
             ReleaseDC(hwnd, hdc); 
-
             ValidateRect(hwnd, &rect); 
-
  
             break;
 
         case WM_KEYDOWN:
-
+                
             switch (virtual_code) {
                 case VK_RIGHT:
                     PlaySound((LPCTSTR)MAKEINTRESOURCE(IDR_WAVE1), hinstance_app, SND_RESOURCE | SND_ASYNC);
@@ -164,23 +157,55 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
                     PostQuitMessage(0);
                     break;
                 case ID_HELP_ABOUT:
-                    MessageBox(hwnd, L"Menu Sound Demo", L"About Sound Menu", MB_OK | MB_ICONEXCLAMATION);
+                    MessageBox(hwnd, L"Так ви не зможете закрити це вікно", L"Що ж сталось?", MB_OK | MB_ICONEXCLAMATION);
 
                     break;
                 default: break;
             }
             break;
+        case WM_CHAR:
 
+            hdc = GetDC(hwnd);
+            GetClientRect(hwnd, &rect);
+            
+            SetTextColor(hdc, RGB(20, 220, 20));
+            SetBkMode(hdc, TRANSPARENT);
+
+            FillRect(hdc, &rect, (HBRUSH)(COLOR_WINDOW + 2));
+
+            sprintf(text, "Draw number = %d ", wm_paint_count);
+            TextOutA(hdc, 10, 20, text, strlen(text));
+
+            sprintf(text3, "WM_CHAR: Character = %c ", ascii_code);
+            TextOutA(hdc, 10, 60, text3, strlen(text3));
+
+            ReleaseDC(hwnd, hdc);
+            ValidateRect(hwnd, &rect);
+            break;
+        case WM_SIZE:
+
+            hdc = GetDC(hwnd);
+            GetClientRect(hwnd, &rect);
+
+            FillRect(hdc, &rect, (HBRUSH)(COLOR_WINDOW + 2));
+
+            SetTextColor(hdc, RGB(20, 220, 20));
+            SetBkMode(hdc, TRANSPARENT);
+          
+            sprintf(text, "Draw number = %d ", wm_paint_count);
+            TextOutA(hdc, 10, 20, text, strlen(text));
+
+            sprintf(text2, "WM_SIZE Called - New Size = %d, %d ", width, height);
+            TextOutA(hdc, 10, 40, text2, strlen(text2));
+
+            ReleaseDC(hwnd, hdc);
+            ValidateRect(hwnd, &rect);
+            break;
         case WM_DESTROY:
             PostQuitMessage(0);
             PlaySound(NULL, hinstance_app, SND_PURGE);
             break;
         default: return DefWindowProc(hwnd, uMsg, wParam, lParam);
     }
-
 }
-
-//void Game_Main(){
-//
-//}
 
--- function.h
+++ function.h
@@ -1,6 +1,10 @@
 #pragma once
 
+#pragma warning(disable : 4996)
+
 #include <windows.h>
+#include <iostream>
+#include <stdio.h>
 #include <ctime>
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
