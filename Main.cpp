b5c677318126576b972e7e293d5a6f824c543124
MihaplAyMF <mihapley@gmail.com>
2023-03-22 21:14:18 +0200

    хлам

--- Main.cpp
+++ Main.cpp
@@ -1,4 +1,4 @@
-#ifndef UNICODE
+﻿#ifndef UNICODE
 #define UNICODE
 #endif 
 
@@ -6,17 +6,23 @@
 #include "resource.h"
 
 #define TIMER_ID_1SEC 1
+#define WINDOW_WIDTH 400
+#define WINDOW_HEIGHT 400
 
-HINSTANCE hinstance_app;
+#define BUTTON_ONE 106
+
+//HINSTANCE hinstance_app;
 POINT ptPrevious;
 COLORREF color = RGB(0, 0, 0);
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow) {
 
-    srand(time(nullptr));
 
-    const wchar_t CLASS_NAME[] = L"Sample Window Class";
-    hinstance_app = hInstance;
+    srand(GetTickCount());
+
+    //hinstance_app = hInstance;
+
+    /*const wchar_t CLASS_NAME[] = L"Sample Window Class";
 
     WNDCLASS wc = { };
 
@@ -44,24 +50,72 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
         NULL        // Additional application data
     );
 
-    if (hwnd == NULL) {
+    HWND hwndButton = CreateWindow(
+        L"BUTTON",  // Predefined class; Unicode assumed 
+        L"OK",      // Button text 
+        WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,  // Styles 
+        10, 10, 100, 24,
+        hwnd,     // Parent window
+        (HMENU)BUTTON_ONE,       // No menu.
+        hInstance,
+        NULL
+    );      // Pointer not needed.
+
+    HWND hEdit = CreateWindow(
+        L"Edit", 
+        NULL, 
+        WS_EX_CLIENTEDGE | WS_BORDER | WS_CHILD | WS_VISIBLE, 
+        200, 
+        200, 
+        100, 
+        100, 
+        hwnd,
+        NULL, 
+        hInstance,
+        0
+    );*/
+
+
+    /*if (hwnd == NULL) {
         return 0;
-    }
+    }*/
 
-    ShowWindow(hwnd, nCmdShow);
+    //ShowWindow(hwnd, nCmdShow);
 
-    MSG msg = { };
+    //MSG msg = { };
+    //HDC hdc = GetDC(hwnd);;
 
-    while (TRUE) {
+    DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), 0, WindowProc, 0);
+
+    /*while (TRUE) {
+
+        DWORD start_time = GetTickCount();
+
         if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
-
             if (msg.message == WM_QUIT) break;
             TranslateMessage(&msg);
             DispatchMessage(&msg);
-
         }
-    }
+    
+        if (++color_change_count >= 100) {
+            color_change_count = 0;
+            if (pen) DeleteObject(pen);
+            pen = CreatePen(PS_SOLID, 1, RGB(rand() % 256, rand() % 256, rand() % 256));
+            SelectObject(hdc, pen);
+        
+        }
 
+
+       // while ((GetTickCount() - start_time) < 16.6); // таймер на 60 фпс
+     
+       /* if (KEYDOWN(VK_ESCAPE))
+            SendMessage(hwnd, WM_CLOSE, 0, 0);
+
+
+    }*/
+
+
+    //ReleaseDC(hwnd, hdc);
     return 0;
 }
 
@@ -83,13 +137,12 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
     switch (uMsg) {
 
         case WM_CREATE:
-            
-            SetTimer(hwnd, TIMER_ID_1SEC, 200, NULL);
+            //SetTimer(hwnd, TIMER_ID_1SEC, 200, NULL);
             break;
         case WM_TIMER:
 
-            switch(wParam){
-                case TIMER_ID_1SEC:
+         /*   switch(wParam){
+               case TIMER_ID_1SEC:
 
                     hdc = GetDC(hwnd);
                     GetClientRect(hwnd, &rect);
@@ -105,21 +158,21 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
                     ReleaseDC(hwnd, hdc);
                     ValidateRect(hwnd, &rect);
 
-                   
+                 
             }
-            break;
+            break;  */
 
         case WM_PAINT:
 
-            hdc = GetDC(hwnd);
+           /* hdc = GetDC(hwnd);
             ReleaseDC(hwnd, hdc);
             GetClientRect(hwnd, &rect);
-            ValidateRect(hwnd, &rect);
+            ValidateRect(hwnd, &rect);*/
             break;
 
         case WM_KEYDOWN:
 
-            switch (virtual_code) {
+            /*switch (virtual_code) {
                 case VK_RIGHT:
                     PlaySound((LPCTSTR)MAKEINTRESOURCE(IDR_WAVE1), hinstance_app, SND_RESOURCE | SND_ASYNC);
                     break;
@@ -129,13 +182,15 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
                 case VK_DOWN:
                     PostQuitMessage(WM_QUIT);
                     break;
-            }
+            }*/
             break;
 
         case WM_COMMAND:
 
-            switch (wParam) {
-                case ID_COLOR_RED: 
+
+
+            /*switch (wParam) {
+                case ID_COLOR_RED:  
                     color = RGB(255, 0, 0);
                     break;
                 case ID_COLOR_YELLOW:
@@ -165,11 +220,13 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
                 case ID_HELP_ABOUT:
                     MessageBox(hwnd, L"It's simple paint", L"About program", MB_OK );
                     break;
+                case BUTTON_ONE:
+                    MessageBox(hwnd, L"You on click button", L"Button", MB_OK);
                 default: break;
-            }
+            }*/
             break;
 
-        case WM_LBUTTONDOWN:
+       /* case WM_LBUTTONDOWN:
 
             hdc = GetDC(hwnd);
             pen = CreatePen(PS_SOLID, 3, color);
@@ -211,20 +268,20 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
                 ReleaseDC(hwnd, hdc);
 
             }
-            break;
+            break;*/
         
         case WM_SIZE:
 
-            hdc = BeginPaint(hwnd, &ps);
+            /*hdc = BeginPaint(hwnd, &ps);
             
-            FillRect(hdc, &ps.rcPaint, CreateSolidBrush(RGB(255, 255, 255)));
+            FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 2));
 
-            EndPaint(hwnd, &ps);
+            EndPaint(hwnd, &ps);*/
             break;
         case WM_DESTROY:
         
             PostQuitMessage(0);
-            PlaySound(NULL, hinstance_app, SND_PURGE);
+            //PlaySound(NULL, hinstance_app, SND_PURGE);
             break;
         
         default: return DefWindowProc(hwnd, uMsg, wParam, lParam);
--- resource.h
+++ resource.h
@@ -6,6 +6,11 @@
 #define IDI_ICON1                       102
 #define IDR_MENU1                       104
 #define IDR_WAVE1                       105
+#define IDD_DIALOG1                     106
+#define IDC_BUTTON1                     1001
+#define IDC_CHECK1                      1003
+#define IDC_CHECK2                      1004
+#define IDC_EDIT2                       1005
 #define ID_FILE_OPEN                    40001
 #define ID_FILE_CLOSE                   40002
 #define ID_FILE_SAVE                    40003
@@ -18,15 +23,15 @@
 #define ID_COLOR_BLACK                  40014
 #define ID_COLOR_WHITE                  40015
 #define ID_COLOR_LIGHTBLUE              40018
-#define ID_COLOR_PURPLE					40019
-
+#define ID_COLOR_PURPLE                 40019
+		
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        106
+#define _APS_NEXT_RESOURCE_VALUE        108
 #define _APS_NEXT_COMMAND_VALUE         40020
-#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_CONTROL_VALUE         1006
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
